5cdcc1d464b92d888406fc594d78bde51d3237c6
nqmihn <qminhqb@gmail.com>
2023-11-09 15:59:16 +0700

    init

---
+++ backend/quiz-api/src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config';
+@Module({
+  imports: [ConfigModule.forRoot({ isGlobal: true }), TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService) => ({
+      type: 'mysql',
+      host: configService.get<string>('DB_HOST'),
+      port: +(configService.get<string>('DB_PORT')),
+      username: configService.get<string>('DB_USERNAME'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_NAME'),
+      entities: [],
+      synchronize: true,
+    }),
+    inject: [ConfigService],
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
